f10cb2037a9ffb0a49224a0c3868eee1549c10c5
Brian Allen <brianallen2401@gmail.com>
2020-04-04 13:45:49 -0600

    formatedd page

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,7 +3,9 @@ const db = require("../models");
 module.exports = function(app) {
   // Get all examples
   app.get("/api/examples", function(request, response) {
-    
+    db.Media.findAll({}).then(function(result) {
+      result.sendFile(path.join(__dirname, "../public/index.html"));
+    });
   });
 
   // Create a new example
