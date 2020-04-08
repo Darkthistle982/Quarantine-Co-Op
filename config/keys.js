544e2b4189762bace26a4aa3304928da2dcaa81c
Swazey Jessup <swazey_jessup@hotmail.com>
2020-04-07 20:04:40 -0600

    passport

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+// add this file to .gitignore
+
+module.exports = {
+    google: {
+    clientID: "710093509245-u1f4vnlq4brv8ugfm103k7q1qgd68nul.apps.googleusercontent.com",
+    clientSecret: "JSs4jqFEg2wGTPMlKNW17QjZ"
+    },
+};
