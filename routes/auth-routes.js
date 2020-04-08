d75b648b58239f01af2b58234b2f47639c797c6f
Brian Allen <brianallen2401@gmail.com>
2020-04-07 21:03:46 -0600

    corrected multiple eslinting errors in multiple files

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -3,23 +3,26 @@ const passport = require("passport");
 
 //auth login
 router.get("/login", (req, res) => {
-    res.render("login");
+  res.render("login");
 });
 
 //auth logout
 router.get("/logout", (req, res) => {
-    //handle with passport
-    res.send("logging out");
+  //handle with passport
+  res.send("logging out");
 });
 
-//auth with google 
-router.get("/google", passport.authenticate("google", {
+//auth with google
+router.get(
+  "/google",
+  passport.authenticate("google", {
     scope: ["profile"]
-}));
+  })
+);
 
 //callback route for google to redirect to
-router.get("/google/redirect", (req, res) =>{
-    res.send("you reached the callback URI");
+router.get("/google/redirect", (req, res) => {
+  res.send("you reached the callback URI");
 });
 
 module.exports = router;
