76d1b5505f823529915bf4a755887f2c12f5c3db
Brian Allen <brianallen2401@gmail.com>
2020-04-08 13:37:01 -0600

    updated styling to show lists. Added in some login logic to test.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,6 +4,7 @@ module.exports = function(app) {
   // Get all examples
   app.get("/", function() {
     db.Media.findAll({}).then(function(result) {
+      console.log(result);
       result.sendFile(path.join(__dirname, "../public/index.html"));
     });
   });
