ee77dc31be8b6cbf706889282c9a24a7c49f6282
Miranda D'Asto <merlinockipella@gmail.com>
2020-04-09 19:01:08 -0600

    Changed user.js under models to fix the hook problem of encoding the password.

--- models/user.js
+++ models/user.js
@@ -4,43 +4,46 @@
 const bcrypt = require("bcrypt-nodejs");
 
 // Creating our User model
-module.exports = function(sequelize, DataTypes) {
-  let User = sequelize.define("User", {
-    // The email cannot be null, and must be a proper email before creation
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        isEmail: true
-      }
-    },
-    // The password cannot be null
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false
+module.exports = function (sequelize, DataTypes) {
+  let User = sequelize.define(
+    "User",
+    {
+      // The email cannot be null, and must be a proper email before creation
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true
+        }
+      },
+      // The password cannot be null
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false
     }
-  });
+  },
 
-  // Below doesn't work, I tried it.
-  // User.prototype.encryptPassword = function(password) {
-  //   return bcrypt.hashSync(password, bcrypt.genSaltSync(10));
-  // };
+  
+    //Hooks are automatic methods that run during various phases of the User Model lifecycle
+    //In this case, before a User is created, it will automatically hash their password
+    {
+      hooks: {
+        beforeCreate: function (user) {
+          user.password = bcrypt.hashSync(
+            user.password,
+            bcrypt.genSaltSync(10),
+            null
+          );
+        }
+      }
+    });
 
   // Creating a custom method for our User model. This will check if an unhashed password entered by the user
   // can be compared to the hashed password stored in our database.
-  User.prototype.validPassword = function(password) {
+  User.prototype.validPassword = function (password) {
     return bcrypt.compareSync(password, this.password);
   };
-
-  // Hooks are automatic methods that run during various phases of the User Model lifecycle
-  // In this case, before a User is created, it will automatically hash their password
-  // User.hook("beforeCreate", function(user) {
-  //   user.password = bcrypt.hashSync(
-  //     user.password,
-  //     bcrypt.genSaltSync(10),
-  //     null
-  //   );
-  // });
+  
   return User;
 };
