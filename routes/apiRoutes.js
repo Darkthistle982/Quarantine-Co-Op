7e9a8e6ed5fc0e7648884d4391060d8006acbb7c
Miranda D'Asto <merlinockipella@gmail.com>
2020-04-09 18:41:12 -0600

    I changed A LOT of stuff. Made the index.js the login page, created a sign-up page, added dependencies for passport, changed the server page. mostly just rearranged it and added notes, but also added things for passport. Commented out Swazey's code on passport-setup and added my own, it may need to be changed. Created login and signup html's and js's. There are probably way more changes and I'm sorry for not being more specific about it...

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,11 +1,51 @@
+// Requiring path so we can use relative routes to our HTML files
+const path = require("path");
+
+// Requiring our custom middleware for checking if a user is logged in
+const isAuthenticated = require("../config/middleware/isAuthenticated");
+
 const db = require("../models");
 
 module.exports = function(app) {
   // Get all examples
-  app.get("/", function() {
+  app.get("/api/examples", function() {
     db.Media.findAll({}).then(function(result) {
-      console.log(result);
       result.sendFile(path.join(__dirname, "../public/index.html"));
     });
   });
+
+  app.get("/", function(req, res) {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/main");
+    }
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+
+  app.get("/signup", function(req, res) {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/main");
+    }
+    res.sendFile(path.join(__dirname, "../public/signup.html"));
+  });
+
+  // Here we'll add our isAuthenticated middleware to this route.
+  // If a user who is not logged in tries to access this route they will be redirected to the signup page
+  app.get("/main", isAuthenticated, function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/main.html"));
+  });
 };
+
+//Below is the original code Brian wrote.
+// const db = require("../models");
+
+// module.exports = function(app) {
+//   // Get all examples
+//   app.get("/", function() {
+//     db.Media.findAll({}).then(function(result) {
+//       console.log(result);
+//       result.sendFile(path.join(__dirname, "../public/index.html"));
+//     });
+//   });
+// };
--- server.js
+++ server.js
@@ -1,37 +1,39 @@
 // require("dotenv").config();
 const express = require("express");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+//requiring passport as we've configure it.
+const passport = require("./config/passport-setup");
 
+//Setting up port
 const PORT = process.env.PORT || 3000;
+const db = require("./models");
+
+//Creating express app and configuring middleware needed for authentiation
 const app = express();
-require("./config/passport-setup");
+// Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static("public"));
+//We need to use sessions to keep track of our user's login status
+app.use(
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+//We need this to hide our api key.
 require("dotenv").config();
 
-//If we are using sequelize we need line 11.
-const db = require("./models");
-const authRoutes = require("./routes/auth-routes");
-
-// Middleware
-app.use(express.static("public"));
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use("/auth", authRoutes);
-
-// set up view engine
+// set up view engine (I don't know what this does -MD)
 app.set("view engine", "ejs");
 
-// Routes
+// Requiring our routes
 require("./routes/apiRoutes")(app);
-
-var syncOptions = { force: false };
-
-// If running a test, set syncOptions.force to true
-// clearing the `testdb`
-if (process.env.NODE_ENV === "test") {
-  syncOptions.force = true;
-}
+require("./routes/auth-routes")(app);
 
 // Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function() {
+db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
     console.log(
       "==> Listening on port %s. Visit http://localhost:%s/ in your browser.",
@@ -42,3 +44,49 @@ db.sequelize.sync(syncOptions).then(function() {
 });
 
 module.exports = app;
+
+//Below is the original code
+// // require("dotenv").config();
+// const express = require("express");
+
+// const PORT = process.env.PORT || 3000;
+// const app = express();
+// require("./config/passport-setup");
+// require("dotenv").config();
+
+// //If we are using sequelize we need line 11.
+// const db = require("./models");
+// const authRoutes = require("./routes/auth-routes");
+
+// // Middleware
+// app.use(express.static("public"));
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+// app.use("/auth", authRoutes);
+
+// // set up view engine
+// app.set("view engine", "ejs");
+
+// // Routes
+// require("./routes/apiRoutes")(app);
+
+// var syncOptions = { force: false };
+
+// // If running a test, set syncOptions.force to true
+// // clearing the `testdb`
+// if (process.env.NODE_ENV === "test") {
+//   syncOptions.force = true;
+// }
+
+// // Starting the server, syncing our models ------------------------------------/
+// db.sequelize.sync(syncOptions).then(function() {
+//   app.listen(PORT, function() {
+//     console.log(
+//       "==> Listening on port %s. Visit http://localhost:%s/ in your browser.",
+//       PORT,
+//       PORT
+//     );
+//   });
+// });
+
+// module.exports = app;
