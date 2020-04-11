312fb86a51e10640251a34b437e2ecde3ffa16af
Miranda D'Asto <merlinockipella@gmail.com>
2020-04-11 11:55:47 -0600

    Changed some of the seeds to true and gave them the names of who checked them out. Also changed quarantine.js so the checkedOutBy and returnByDate allowed null.

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,10 +1,10 @@
-INSERT INTO media (title, authorCreator, genre, rating, mediaType, checkedOut)
-    VALUES("Harry Potter and the Sorcerer's Stone", "J.K. Rowling", "Fantasty", "Young Adult", "Book", false),
+INSERT INTO media (title, authorCreator, genre, rating, mediaType, checkedOut, checkedOutBy)
+    VALUES("Harry Potter and the Sorcerer's Stone", "J.K. Rowling", "Fantasty", "Young Adult", "Book", true, "Brian Allen"),
             ("The Hunger Games (The Hunger Games, #1)", "Suzanne Collins", "Science Fiction", "Young Adult", "Book", false),
             ("Fifty Shades of Grey", "E.L. James", "Romance", "Adult", "Book", false),
             ("Pride & Prejudice", "Jane Austen", "Romance", "Adult", "Book", false),
             ("The Eye of the World (The Wheel of Time, #1)", "Robert Jordan", "Fantasy", "Adult", "Book", false),
-            ("The Final Empire (Mistborn, #1)", "Brandon Sanderson", "Fantasy", "Young Adult", "Book", false),
+            ("The Final Empire (Mistborn, #1)", "Brandon Sanderson", "Fantasy", "Young Adult", "Book", true, "Swazey Jessup"),
             ("In Bruges", "Martin McDonagh", "Comedy", "R", "Movie", false),
             ("The Grand Budapest Hotel", "Wes Anderson", "Adventure", "R", "Movie", false),
             ("The Hunt for Red October", "John McTiernan", "Action", "PG", "Movie", false),
@@ -12,6 +12,6 @@ INSERT INTO media (title, authorCreator, genre, rating, mediaType, checkedOut)
             ("Sex Education", "Laurie Nunn", "Comedy", "TV-MA", "TV-Show", false),
             ("I Am Not Okay with This", "Jonathan Entwistle & Christy Hall", "Comedy", "TV-MA", "TV-Show", false),
             ("Avatar: The Last Airbender", "Michael Dante DiMartino & Bryan Konietzko", "Animation", "TV-Y7-FV", "TV-Show", false),
-            ("Awesomenauts", "Ronimo Games", "MOBA", "T", "Video Game", false),
-            ("Terraria", "Re-Logic", "Action-adventure game", "T", "Video Game", false),
+            ("Awesomenauts", "Ronimo Games", "MOBA", "T", "Video Game", true, "Matt Jimenez"),
+            ("Terraria", "Re-Logic", "Action-adventure game", "T", "Video Game", true, "Miranda D'Asto"),
             ("The Witcher", "CD Projekt", "Action role-playing","M", "Video Game",false);
