51f0133f6e4c1219c6ec63d04e0ab94f7a1fb118
Brian Allen <brianallen2401@gmail.com>
2020-04-11 14:04:30 -0600

    deleted test folder, and login ejs. updated server.js and main.html removed tested onload

--- server.js
+++ server.js
@@ -25,9 +25,6 @@ app.use(passport.session());
 //We need this to hide our api key.
 require("dotenv").config();
 
-// set up view engine (I don't know what this does -MD)
-app.set("view engine", "ejs");
-
 // Requiring our routes
 require("./routes/apiRoutes")(app);
 require("./routes/auth-routes")(app);
@@ -44,49 +41,3 @@ db.sequelize.sync().then(function() {
 });
 
 module.exports = app;
-
-//Below is the original code
-// // require("dotenv").config();
-// const express = require("express");
-
-// const PORT = process.env.PORT || 3000;
-// const app = express();
-// require("./config/passport-setup");
-// require("dotenv").config();
-
-// //If we are using sequelize we need line 11.
-// const db = require("./models");
-// const authRoutes = require("./routes/auth-routes");
-
-// // Middleware
-// app.use(express.static("public"));
-// app.use(express.urlencoded({ extended: false }));
-// app.use(express.json());
-// app.use("/auth", authRoutes);
-
-// // set up view engine
-// app.set("view engine", "ejs");
-
-// // Routes
-// require("./routes/apiRoutes")(app);
-
-// var syncOptions = { force: false };
-
-// // If running a test, set syncOptions.force to true
-// // clearing the `testdb`
-// if (process.env.NODE_ENV === "test") {
-//   syncOptions.force = true;
-// }
-
-// // Starting the server, syncing our models ------------------------------------/
-// db.sequelize.sync(syncOptions).then(function() {
-//   app.listen(PORT, function() {
-//     console.log(
-//       "==> Listening on port %s. Visit http://localhost:%s/ in your browser.",
-//       PORT,
-//       PORT
-//     );
-//   });
-// });
-
-// module.exports = app;
