37685823f3eb1e88ff7005c3c5e81466d499670b
Brian Allen <brianallen2401@gmail.com>
2020-04-14 12:11:27 -0600

    fixed linting error

--- public/js/main.js
+++ public/js/main.js
@@ -38,8 +38,7 @@ $(document).ready(function() {
       rating: $("#rating")
         .val()
         .trim(),
-      mediaType: $("#mediaType")
-        .val()
+      mediaType: $("#mediaType").val()
     };
     $.post("/api/addNew", newMedia, function() {
       loadAllMedia();
